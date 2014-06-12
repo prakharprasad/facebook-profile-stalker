d57ac005f6e85b5b9bb2b383da95d70a3de28148
Prakhar Prasad <prakhar@prakharprasad.com>
2014-06-12 15:03:00 +0530

    Initial Commit

---
+++ ajax.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(0);
+class StalkErr extends Exception {}
+header("Content-Type: application/json");
+header("X-FRAME-OPTIONS: SAMEORIGIN");
+define("ERR_URI", "Invalid URL or Profile: Are you sure it's a valid link: \n\nhttps://www.facebook.com/username\nhttp://m.facebook.com/profile.php?id=123");
+define("ERR_PAGE","The link you provided is of a page not a Facebook \"profile\"\n\nPlease try again with a valid profile");
+define("ERR_UPSTREAM","Upstream didn't respond, please refresh the page and try again!");
+function getId($id){
+	if(!preg_match("/^(http|https)/i", $id)) $id = "https://".$id;
+	$hosts = array("www.facebook.com","zero.facebook.com","iphone.facebook.com","0.facebook.com","facebook.com","touch.facebook.com","m.facebook.com","mbasic.facebook.com");
+	$id = parse_url($id);
+	if(!in_array($id["host"],$hosts)) return -1;
+	parse_str($id['query']);
+	if(!is_numeric($id)) $id  = substr($id['path'],1);
+	$x = json_decode(file_get_contents("http://graph.facebook.com/".$id));
+    if(!is_numeric($x->id)) return -2;
+    return $x;
+}
+function main(){
+	try {
+		$id = @getId($_GET['id']);
+		if(intval($id->id) <= 0) throw new StalkErr(ERR_URI);
+		if($id->likes) throw new StalkErr(ERR_PAGE);
+		$url = get_headers("http://graph.facebook.com/".$id->id."/picture?type=large",1)["Location"];
+		if(!$url)  throw new StalkErr(ERR_UPSTREAM);
+		echo json_encode(array("title" => htmlspecialchars($id->name,ENT_QUOTES)." | ".ucfirst(htmlspecialchars($id->gender,ENT_QUOTES)),
+			 "data"=>"data:image/jpg;base64,".base64_encode(file_get_contents(preg_replace("/[0-9]{3}x[0-9]{3}/i","720x720",$url)))));
+	    }
+	catch(Exception $e)
+	 {
+		echo json_encode(array("error" => $e->getMessage()."\n\nThis was caught at ".$e->getLine()));
+	 } }
+main(); //Like a C programmer ;)
+?>
