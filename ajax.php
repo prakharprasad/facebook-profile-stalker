f3e9dcbc66690af0dabf07b456dcfeb37c168c7b
Prakhar Prasad <prakhar@prakharprasad.com>
2014-06-21 03:04:04 +0530

    Fixes a bug when resizing failed
    When there is no “resized” picture available, upstream throws an error
    which didn’t get handled by the code, resulting in a broken image.
    
    This commit fixes this by using the maximum size returned by the Graph
    API as a fallback technique when upstream throws an error.

--- ajax.php
+++ ajax.php
@@ -1,11 +1,11 @@
 <?php
 error_reporting(0);
 class StalkErr extends Exception {}
-header("Content-Type: application/json");
-header("X-FRAME-OPTIONS: SAMEORIGIN");
 define("ERR_URI", "Invalid URL or Profile: Are you sure it's a valid link: \n\nhttps://www.facebook.com/username\nhttp://m.facebook.com/profile.php?id=123");
 define("ERR_PAGE","The link you provided is of a page not a Facebook \"profile\"\n\nPlease try again with a valid profile");
-define("ERR_UPSTREAM","Upstream didn't respond, please refresh the page and try again!");
+define("ERR_UPSTREAM_NORESPONSE","Upstream didn't respond, please refresh the page and try again!");
+header("Content-Type: application/json");
+header("X-FRAME-OPTIONS: SAMEORIGIN");
 function getId($id){
 	if(!preg_match("/^(http|https)/i", $id)) $id = "https://".$id;
 	$hosts = array("www.facebook.com","zero.facebook.com","iphone.facebook.com","0.facebook.com","facebook.com","touch.facebook.com","m.facebook.com","mbasic.facebook.com");
@@ -23,13 +23,15 @@ function main(){
 		if(intval($id->id) <= 0) throw new StalkErr(ERR_URI);
 		if($id->likes) throw new StalkErr(ERR_PAGE);
 		$url = get_headers("http://graph.facebook.com/".$id->id."/picture?type=large",1)["Location"];
-		if(!$url)  throw new StalkErr(ERR_UPSTREAM);
+		if(!$url)  throw new StalkErr(ERR_UPSTREAM_NORESPONSE);
+		$image = file_get_contents(preg_replace("/[0-9]{3}x[0-9]{3}/i","720x720",$url));
+		if(!@getimagesizefromstring($image)) $image = file_get_contents($url); //fallback to Graph API maximum, when resizing fails
 		echo json_encode(array("title" => htmlspecialchars($id->name,ENT_QUOTES)." | ".ucfirst(htmlspecialchars($id->gender,ENT_QUOTES)),
-			 "data"=>"data:image/jpg;base64,".base64_encode(file_get_contents(preg_replace("/[0-9]{3}x[0-9]{3}/i","720x720",$url)))));
+			 "data"=>"data:image/jpg;base64,".base64_encode($image)));
 	    }
 	catch(Exception $e)
 	 {
-		echo json_encode(array("error" => $e->getMessage()."\n\nThis was caught at ".$e->getLine()));
+		echo json_encode(array("error" => $e->getMessage()));
 	 } }
 main(); //Like a C programmer ;)
 ?>
